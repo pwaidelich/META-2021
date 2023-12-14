a669ceb4917825b1ef6c8fae6f6d730f1fc5dfaf
pwaidelich <46569171+pwaidelich@users.noreply.github.com>
2023-12-14 17:51:43 +0100

    lossfactors for ind. TPs

--- src/extract_lossfactors.jl
+++ src/extract_lossfactors.jl
@@ -9,13 +9,27 @@ using Random
 # dimensions are taken from src/basemodel.jl
 countries = CSV.read("../data/pattern-scaling.csv", DataFrame).Country
 years = collect(2010:2200)
+seed_used = 25081776
 
-function run_equityvaluation_mc(mc_samplesize = 50; rcp = "RCP4.5", ssp = "SSP2", persist::Union{Float64, AbstractString} = "Distribution", seed_used = 25081776,
-                                tp_used = "all",
-                                dir_output = "C:/Users/pwaidelich/Downloads/GitHub - Local/equity_var_climate/analysis")
-    ##  Load model and select configuration
-    Random.seed!(seed_used)
+global mc_samplesize = 500
 
+Random.seed!(seed_used)
+global mc_draws_used = getsim(mc_samplesize,
+                # NOTE: we use Hope & Schaefer PCF for consistency with Dietz et al 2021
+                # this means that there is effectively no MC draws for the PCF component
+                # alternatively, set this to 'Kessler probabilistic'
+                "Fit of Hope and Schaefer (2016)", # PCF
+                "Distribution", # AMAZ
+                "Distribution", # GIS
+                "Distribution",  # WAIS
+                "Distribution", # SAF
+               true, # persit
+               false, # emuc
+               false) # prtp
+
+function run_equityvaluation_mc(mc_samplesize = 500; rcp = "RCP4.5", ssp = "SSP2", persist::Union{Float64, AbstractString} = "Distribution", seed_used = 25081776,
+                                tp_used = "all", tdamage = "BHM",
+                                dir_output = "C:/Users/pwaidelich/Downloads/GitHub - Local/equity_var_climate/data/META MC results")
     # check the function inputs
     if (isa(persist, AbstractString) && persist != "Distribution")
         error("persist can only be set to Distribution if it is a string")
@@ -29,6 +43,7 @@ function run_equityvaluation_mc(mc_samplesize = 50; rcp = "RCP4.5", ssp = "SSP2"
     # function base_model(; rcp="CP-Base", ssp="SSP2", co2="Expectation", ch4="default", warming="Best fit multi-model mean", tdamage="none", slrdamage="none")
     # function full_model(; rcp="RCP4.5", ssp="SSP2", co2="Expectation", ch4="default", warming="Best fit multi-model mean", tdamage="pointestimate", slrdamage="mode", saf="Distribution mean", interaction=true, pcf="Fit of Hope and Schaefer (2016)", omh="Whiteman et al. beta 20 years", amaz="Cai et al. central value", gis="Nordhaus central value", wais="Value", ism="Value", amoc="IPSL", nonmarketdamage=false)
 
+    Random.seed!(seed_used)
     global model = full_model(;
         rcp=rcp,
         ssp=ssp,
@@ -49,30 +64,21 @@ function run_equityvaluation_mc(mc_samplesize = 50; rcp = "RCP4.5", ssp = "SSP2"
         update_param!(model, :Consumption, :damagepersist, persist)
     end
 
-    Random.seed!(seed_used)
-    draws = getsim(mc_samplesize, ifelse(tp_used == "all" || occursin("PCF", tp_used), "Kessler probabilistic", "none"), # PCF
-                   ifelse(tp_used == "all" || occursin("AMAZ", tp_used), "Distribution", "none"), # AMAZ
-                   ifelse(tp_used == "all" || occursin("GIS", tp_used), "Distribution", "none"), # GIS
-                   ifelse(tp_used == "all" || occursin("WAIS", tp_used), "Distribution", "none"), # WAIS
-                   ifelse(tp_used == "all" || occursin("SAF", tp_used), "Distribution", "none"), # SAF
-                   # if persist is 'Distribution' (and hence a string), we set the persit argument to true to draw from the MC distribution
-                   isa(persist, AbstractString), # persit
-                   false, # emuc
-                   false) # prtp
-
-    results = runsim(model, draws, tp_used == "all" || occursin("ISM", tp_used), # ism_used
-                                   tp_used == "all" || occursin("OMH", tp_used), # omh_used
-                                   tp_used == "all" || occursin("AMOC", tp_used), # amoc_used
-                                   ifelse(tp_used == "all" || occursin("AMAZ", tp_used), "Distribution", "none"), # AMAZ
-                                   ifelse(tp_used == "all" || occursin("WAIS", tp_used), "Distribution", "none") # WAIS
-                                   ) # save all random variables)
+    results = runsim(model, mc_draws_used,
+                     tp_used == "all" || occursin("ISM", tp_used), # ism_used
+                     tp_used == "all" || occursin("OMH", tp_used), # omh_used
+                     tp_used == "all" || occursin("AMOC", tp_used), # amoc_used
+                     tp_used == "all" || occursin("SAF", tp_used), # saf_used
+                     ifelse(tp_used == "all" || occursin("AMAZ", tp_used), "Distribution", "none"), # AMAZ
+                     ifelse(tp_used == "all" || occursin("WAIS", tp_used), "Distribution", "none") # WAIS
+                     ) # save all random variables)
 
     # save out as a pickle-like HDF object
-    save_object(string(dir_output, "/lossfactors/jld2_files/", mc_samplesize,
+    save_object(string(dir_output, "/jld2_files/", mc_samplesize,
                         # remove special characters from scenario names that can cause filename issues: ".", "/"
                         "_", replace(replace(rcp, "." => ""), "/" => ""), replace(ssp, "." => ""),
                         "_persist", persist,
-                        "_tip", tp_used, ".jld2"),
+                        "_tip", tp_used, "_tdamage", tdamage, ".jld2"),
                 results)
 
     # return the results
@@ -106,11 +112,12 @@ end
 # convert_mcresults_to_longdf(temp, :Consumption_lossfactor_temp)
 
 function extract_lossfactors(mc_samplesize=nothing; rcp=nothing, ssp=nothing, persist=nothing, tp_used = "all",
-                             dir_output = "C:/Users/pwaidelich/Downloads/GitHub - Local/equity_var_climate/analysis")
+                             tdamage = "BHM",
+                             dir_output = "C:/Users/pwaidelich/Downloads/GitHub - Local/equity_var_climate/data/META MC results")
 
     # combine the parameter combinations into an identifier string
     parameter_id = string("n", mc_samplesize, "_", replace(replace(rcp, "." => ""), "/" => ""), replace(ssp, "." => ""),
-                          "_persist", persist, "_tip", tp_used)
+                          "_persist", persist, "_tip", tp_used, "_tdamage", tdamage)
 
     # print out the parameters to track progress
     print(parameter_id)
@@ -126,7 +133,8 @@ function extract_lossfactors(mc_samplesize=nothing; rcp=nothing, ssp=nothing, pe
                                      rcp =  rcp,
                                      ssp = ssp,
                                      tp_used = tp_used,
-                                     persist = persist)
+                                     persist = persist,
+                                     tdamage = tdamage)
 
     # loop through different damage factors of interest
     for lossfactor_name_jj in ["lossfactor_conspc", "lossfactor_persistence",
@@ -142,6 +150,7 @@ function extract_lossfactors(mc_samplesize=nothing; rcp=nothing, ssp=nothing, pe
         insertcols!(df_gdppc_damagefactor, :ssp => ssp)
         insertcols!(df_gdppc_damagefactor, :tp_used => tp_used)
         insertcols!(df_gdppc_damagefactor, :persist => persist)
+        insertcols!(df_gdppc_damagefactor, :tdamage => tdamage)
 
         # export as CSV
         CSV.write(string(dir_output, "/lossfactors/", lossfactor_name_jj, "/",
@@ -153,23 +162,23 @@ end
 # extract_lossfactors(50, rcp = "RCP4.5", ssp = "SSP2", persist = "Distribution",
 #                    tp_used = "all")
 
-# set the overall Monte Carlo sample size
-mc_samplesize = 50
-
 # run MC across scenarios & relevant settings, saving out results as .jld2 files
 for (rcp_jj, ssp_jj) in [("RCP3-PD/2.6", "SSP2"), ("RCP4.5", "SSP2"), ("RCP8.5", "SSP2"),
                          ("RCP3-PD/2.6", "SSP5"), ("RCP4.5", "SSP5"), ("RCP8.5", "SSP5")]
     for persist_jj in ["Distribution", 0., 1., 0.5]
         for tp_used_jj in ["all", "none", "SAF", "PCF", "GIS", "WAIS", "AMAZ", "OMH", "ISM", "AMOC"]
+            for tdamage_jj in ["BHM"]
 
-            # skip parameter combinations that are not of interest
-            if !(tp_used_jj in ["all", "none"]) && ((rcp_jj, ssp_jj) != ("RCP4.5", "SSP2") || persist_jj != "Distribution")
-                continue
-            elseif persist_jj != "Distribution" && ((rcp_jj, ssp_jj) != ("RCP4.5", "SSP2"))
-                continue
+                # skip parameter combinations that are not of interest
+                if !(tp_used_jj in ["all", "none"]) && ((rcp_jj, ssp_jj) != ("RCP4.5", "SSP2") || persist_jj != "Distribution")
+                    continue
+                elseif persist_jj != "Distribution" && ((rcp_jj, ssp_jj) != ("RCP4.5", "SSP2"))
+                    continue
+                end
+
+                extract_lossfactors(mc_samplesize, rcp=rcp_jj, ssp=ssp_jj, persist=persist_jj, tp_used=tp_used_jj,
+                                    tdamage = tdamage_jj)
             end
-
-            extract_lossfactors(mc_samplesize, rcp=rcp_jj, ssp=ssp_jj, persist=persist_jj, tp_used=tp_used_jj)
         end
     end
 end
