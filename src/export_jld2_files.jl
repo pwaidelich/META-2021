08c48ef5fd49b2fd89bc319359920cb37f633b41
pwaidelich <46569171+pwaidelich@users.noreply.github.com>
2024-03-25 13:35:47 +0100

    incl no-OMH for export_jld2_files.jl

--- src/export_jld2_files.jl
+++ src/export_jld2_files.jl
@@ -13,13 +13,11 @@ years = collect(2010:2200)
 dir_meta_results = "C:/Users/pwaidelich/Downloads/GitHub - Local/equity_var_climate/data/META MC results"
 
 # Open the .jld2 file
-file = jldopen(string(dir_meta_results, "/jld2_files/500_RCP45SSP2_persistDistribution_tipall_tdamageBHM.jld2"), "r")  # Replace "your_file.jld2" with your file's name
+file = jldopen(string(dir_meta_results, "/jld2_files/2000_RCP45SSP2_persistDistribution_tipall_tdamagebhm_distribution_omhdefault.jld2"), "r")  # Replace "your_file.jld2" with your file's name
 
 # inspect the keys in it
 keys(file)
 # -> "single_stored_object"
-
-
 # NOTE: the resulting object is a vector with length = # of MC draws. Each element then is a dictionary
 
 function extract_global_temperature(filepath = String[])
@@ -40,29 +38,57 @@ function extract_global_temperature(filepath = String[])
 end
 
 # RCP4.5 with TPs
-df_T_AT_tipall_RCP45 = extract_global_temperature(string(dir_meta_results, "/jld2_files/500_RCP45SSP2_persistDistribution_tipall_tdamageBHM.jld2"))
-CSV.write(string(dir_meta_results, "/T_AT/T_AT_n500_RCP45SSP2_tipall.csv"), df_T_AT_tipall_RCP45)
+df_T_AT_tipall_RCP45 = extract_global_temperature(string(dir_meta_results, "/jld2_files/2000_RCP45SSP2_persistDistribution_tipall_tdamagebhm_distribution_omhdefault.jld2"))
+CSV.write(string(dir_meta_results, "/T_AT/T_AT_n2000_RCP45SSP2_tipall.csv"), df_T_AT_tipall_RCP45)
+
+# RCP4.5 with TPs excl OMH
+df_T_AT_tipall_omhnone_RCP45 = extract_global_temperature(string(dir_meta_results, "/jld2_files/2000_RCP45SSP2_persistDistribution_tipall_tdamagebhm_distribution_omhnone.jld2"))
+CSV.write(string(dir_meta_results, "/T_AT/T_AT_n2000_RCP45SSP2_tipall_omhnone.csv"), df_T_AT_tipall_omhnone_RCP45)
 
 # RCP 4.5 w/o TPs
-df_T_AT_tipnone_RCP45 = extract_global_temperature(string(dir_meta_results, "/jld2_files/500_RCP45SSP2_persistDistribution_tipnone_tdamageBHM.jld2"))
-CSV.write(string(dir_meta_results, "/T_AT/T_AT_n500_RCP45SSP2_tipnone.csv"), df_T_AT_tipnone_RCP45)
+df_T_AT_tipnone_RCP45 = extract_global_temperature(string(dir_meta_results, "/jld2_files/2000_RCP45SSP2_persistDistribution_tipnone_tdamagebhm_distribution_omhdefault.jld2"))
+CSV.write(string(dir_meta_results, "/T_AT/T_AT_n2000_RCP45SSP2_tipnone.csv"), df_T_AT_tipnone_RCP45)
 
 
 # RCP8.5 with TPs
-df_T_AT_tipall_RCP85 = extract_global_temperature(string(dir_meta_results, "/jld2_files/500_RCP85SSP2_persistDistribution_tipall_tdamageBHM.jld2"))
-CSV.write(string(dir_meta_results, "/T_AT/T_AT_n500_RCP85SSP2_tipall.csv"), df_T_AT_tipall_RCP85)
+df_T_AT_tipall_RCP85 = extract_global_temperature(string(dir_meta_results, "/jld2_files/2000_RCP85SSP2_persistDistribution_tipall_tdamagebhm_distribution_omhdefault.jld2"))
+CSV.write(string(dir_meta_results, "/T_AT/T_AT_n2000_RCP85SSP2_tipall.csv"), df_T_AT_tipall_RCP85)
+
+# RCP8.5 with TPs excl. OMH
+df_T_AT_tipall_RCP85 = extract_global_temperature(string(dir_meta_results, "/jld2_files/2000_RCP85SSP2_persist1.0_tipall_tdamagecoacch_central_omhnone.jld2"))
+CSV.write(string(dir_meta_results, "/T_AT/T_AT_n2000_RCP85SSP2_tipall_omhnone.csv"), df_T_AT_tipall_RCP85)
 
 # RCP 8.5 w/o TPs
-df_T_AT_tipnone_RCP85 = extract_global_temperature(string(dir_meta_results, "/jld2_files/500_RCP85SSP2_persistDistribution_tipnone_tdamageBHM.jld2"))
-CSV.write(string(dir_meta_results, "/T_AT/T_AT_n500_RCP85SSP2_tipnone.csv"), df_T_AT_tipnone_RCP85)
+df_T_AT_tipnone_RCP85 = extract_global_temperature(string(dir_meta_results, "/jld2_files/2000_RCP85SSP2_persistDistribution_tipnone_tdamagebhm_distribution_omhdefault.jld2"))
+CSV.write(string(dir_meta_results, "/T_AT/T_AT_n2000_RCP85SSP2_tipnone.csv"), df_T_AT_tipnone_RCP85)
 
 # RCP2.6 with TPs
-df_T_AT_tipall_RCP26 = extract_global_temperature(string(dir_meta_results, "/jld2_files/500_RCP3-PD26SSP2_persistDistribution_tipall_tdamageBHM.jld2"))
-CSV.write(string(dir_meta_results, "/T_AT/T_AT_n500_RCP3-PD26SSP2_tipall.csv"), df_T_AT_tipall_RCP26)
+df_T_AT_tipall_RCP26 = extract_global_temperature(string(dir_meta_results, "/jld2_files/2000_RCP3-PD26SSP2_persistDistribution_tipall_tdamagebhm_distribution_omhdefault.jld2"))
+CSV.write(string(dir_meta_results, "/T_AT/T_AT_n2000_RCP3-PD26SSP2_tipall.csv"), df_T_AT_tipall_RCP26)
+
+# RCP2.6 with TPs excl. OMH
+df_T_AT_tipall_RCP26 = extract_global_temperature(string(dir_meta_results, "/jld2_files/2000_RCP3-PD26SSP2_persist1.0_tipall_tdamagecoacch_central_omhnone.jld2"))
+CSV.write(string(dir_meta_results, "/T_AT/T_AT_n2000_RCP3-PD26SSP2_tipall_omhnone.csv"), df_T_AT_tipall_RCP26)
 
 # RCP 2.6 w/o TPs
-df_T_AT_tipnone_RCP26 = extract_global_temperature(string(dir_meta_results, "/jld2_files/500_RCP3-PD26SSP2_persistDistribution_tipnone_tdamageBHM.jld2"))
-CSV.write(string(dir_meta_results, "/T_AT/T_AT_n500_RCP3-PD26SSP2_tipnone.csv"), df_T_AT_tipnone_RCP26)
+df_T_AT_tipnone_RCP26 = extract_global_temperature(string(dir_meta_results, "/jld2_files/2000_RCP3-PD26SSP2_persistDistribution_tipnone_tdamagebhm_distribution_omhdefault.jld2"))
+CSV.write(string(dir_meta_results, "/T_AT/T_AT_n2000_RCP3-PD26SSP2_tipnone.csv"), df_T_AT_tipnone_RCP26)
+
+
+### individual TPs
+
+for tip_jj in ["AMAZ", "AMOC", "GIS", "WAIS", "ISM", "PCF", "OMH"]
+
+    print(tip_jj)
+
+    CSV.write(string(dir_meta_results, "/T_AT/T_AT_n2000_RCP45SSP2_tip", tip_jj, ".csv"),
+              extract_global_temperature(string(dir_meta_results, "/jld2_files/2000_RCP45SSP2_persistDistribution_tip", tip_jj, "_tdamagebhm_distribution_omhdefault.jld2")))
+
+end
+
+
+
+
 
 # repeat the same for the global sea-level rise variable
 function extract_global_SLR(filepath = String[])
@@ -83,25 +109,47 @@ function extract_global_SLR(filepath = String[])
 end
 
 # RCP 4.5 w/ TPs
-df_SLR_tipall_RCP45 = extract_global_SLR(string(dir_meta_results, "/jld2_files/500_RCP45SSP2_persistDistribution_tipall_tdamageBHM.jld2"))
-CSV.write(string(dir_meta_results, "/SLR/SLR_n500_RCP45SSP2_tipall.csv"), df_SLR_tipall_RCP45)
+df_SLR_tipall_RCP45 = extract_global_SLR(string(dir_meta_results, "/jld2_files/2000_RCP45SSP2_persistDistribution_tipall_tdamagebhm_distribution_omhdefault.jld2"))
+CSV.write(string(dir_meta_results, "/SLR/SLR_n2000_RCP45SSP2_tipall.csv"), df_SLR_tipall_RCP45)
+
+# RCP 4.5 w/ TPs excl. OMH
+df_SLR_tipall_omhnone_RCP45 = extract_global_SLR(string(dir_meta_results, "/jld2_files/2000_RCP45SSP2_persistDistribution_tipall_tdamagebhm_distribution_omhnone.jld2"))
+CSV.write(string(dir_meta_results, "/SLR/SLR_n2000_RCP45SSP2_tipall_omhnone.csv"), df_SLR_tipall_omhnone_RCP45)
 
 # RCP 4.5 w/o TPs
-df_T_AT_tipnone_RCP45 = extract_global_SLR(string(dir_meta_results, "/jld2_files/500_RCP45SSP2_persistDistribution_tipnone_tdamageBHM.jld2"))
-CSV.write(string(dir_meta_results, "/SLR/SLR_n500_RCP45SSP2_tipnone.csv"), df_T_AT_tipnone_RCP45)
+df_T_AT_tipnone_RCP45 = extract_global_SLR(string(dir_meta_results, "/jld2_files/2000_RCP45SSP2_persistDistribution_tipnone_tdamagebhm_distribution_omhdefault.jld2"))
+CSV.write(string(dir_meta_results, "/SLR/SLR_n2000_RCP45SSP2_tipnone.csv"), df_T_AT_tipnone_RCP45)
 
 # RCP 8.5 w/ TPs
-df_SLR_tipall_RCP85 = extract_global_SLR(string(dir_meta_results, "/jld2_files/500_RCP85SSP2_persistDistribution_tipall_tdamageBHM.jld2"))
-CSV.write(string(dir_meta_results, "/SLR/SLR_n500_RCP85SSP2_tipall.csv"), df_SLR_tipall_RCP85)
+df_SLR_tipall_RCP85 = extract_global_SLR(string(dir_meta_results, "/jld2_files/2000_RCP85SSP2_persistDistribution_tipall_tdamagebhm_distribution_omhdefault.jld2"))
+CSV.write(string(dir_meta_results, "/SLR/SLR_n2000_RCP85SSP2_tipall.csv"), df_SLR_tipall_RCP85)
+
+# RCP 8.5 w/ TPs excl. OMH
+df_SLR_tipall_RCP85_omhnone = extract_global_SLR(string(dir_meta_results, "/jld2_files/2000_RCP85SSP2_persist1.0_tipall_tdamagecoacch_central_omhnone.jld2"))
+CSV.write(string(dir_meta_results, "/SLR/SLR_n2000_RCP85SSP2_tipall_omhnone.csv"), df_SLR_tipall_RCP85_omhnone)
 
 # RCP 8.5 w/o TPs
-df_T_AT_tipnone_RCP85 = extract_global_SLR(string(dir_meta_results, "/jld2_files/500_RCP85SSP2_persistDistribution_tipnone_tdamageBHM.jld2"))
-CSV.write(string(dir_meta_results, "/SLR/SLR_n500_RCP85SSP2_tipnone.csv"), df_T_AT_tipnone_RCP85)
+df_T_AT_tipnone_RCP85 = extract_global_SLR(string(dir_meta_results, "/jld2_files/2000_RCP85SSP2_persistDistribution_tipnone_tdamagebhm_distribution_omhdefault.jld2"))
+CSV.write(string(dir_meta_results, "/SLR/SLR_n2000_RCP85SSP2_tipnone.csv"), df_T_AT_tipnone_RCP85)
 
 # RCP 2.6 w/ TPs
-df_SLR_tipall_RCP26 = extract_global_SLR(string(dir_meta_results, "/jld2_files/500_RCP3-PD26SSP2_persistDistribution_tipall_tdamageBHM.jld2"))
-CSV.write(string(dir_meta_results, "/SLR/SLR_n500_RCP3-PD26SSP2_tipall.csv"), df_SLR_tipall_RCP26)
+df_SLR_tipall_RCP26 = extract_global_SLR(string(dir_meta_results, "/jld2_files/2000_RCP3-PD26SSP2_persistDistribution_tipall_tdamagebhm_distribution_omhdefault.jld2"))
+CSV.write(string(dir_meta_results, "/SLR/SLR_n2000_RCP3-PD26SSP2_tipall.csv"), df_SLR_tipall_RCP26)
+
+# RCP 2.6 w/ TPs excl. OMH
+df_SLR_tipall_RCP26_omhnone = extract_global_SLR(string(dir_meta_results, "/jld2_files/2000_RCP3-PD26SSP2_persist1.0_tipall_tdamagecoacch_central_omhnone.jld2"))
+CSV.write(string(dir_meta_results, "/SLR/SLR_n2000_RCP3-PD26SSP2_tipall_omhnone.csv"), df_SLR_tipall_RCP26_omhnone)
 
 # RCP 2.6 w/o TPs
-df_T_AT_tipnone_RCP26 = extract_global_SLR(string(dir_meta_results, "/jld2_files/500_RCP3-PD26SSP2_persistDistribution_tipnone_tdamageBHM.jld2"))
-CSV.write(string(dir_meta_results, "/SLR/SLR_n500_RCP3-PD26SSP2_tipnone.csv"), df_T_AT_tipnone_RCP26)
+df_T_AT_tipnone_RCP26 = extract_global_SLR(string(dir_meta_results, "/jld2_files/2000_RCP3-PD26SSP2_persistDistribution_tipnone_tdamagebhm_distribution_omhdefault.jld2"))
+CSV.write(string(dir_meta_results, "/SLR/SLR_n2000_RCP3-PD26SSP2_tipnone.csv"), df_T_AT_tipnone_RCP26)
+
+### individual TPs
+for tip_jj in ["AMAZ", "AMOC", "GIS", "WAIS", "ISM", "PCF", "OMH"]
+
+    print(tip_jj)
+
+    CSV.write(string(dir_meta_results, "/SLR/SLR_n2000_RCP45SSP2_tip", tip_jj, ".csv"),
+              extract_global_SLR(string(dir_meta_results, "/jld2_files/2000_RCP45SSP2_persistDistribution_tip", tip_jj, "_tdamagebhm_distribution_omhdefault.jld2")))
+
+end
